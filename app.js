ac2bc31dedbee2168cacaabc4f3f5d2efadb70e3
Ewuradjoa Dadzie <ewura.dadzie@gmail.com>
2023-02-06 18:06:33 +0000

    display first user

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'john@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/11.jpg'
+        }
+    }
+})
+
+app.mount('#app')
